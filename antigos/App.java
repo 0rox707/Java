19aabac7192949fbc1987683f065f14f1b9caa78
JoãoG <J.Gomes0963@gmail.com>
2024-11-09 19:14:42 -0300

    Alteração de alguns arquivos, mas nada siginificativo

--- antigos/App.java
+++ antigos/App.java
@@ -1,26 +1,13 @@
-import java.io.IOException;
+// import java.io.IOException;
 
 public class App {
     
     public static void main(String[] args)throws InterruptedException {
-        Menu mn=new Menu();
-        //mn.menu();
-        System.out.println(System.getProperty("os.name").contains("Mac"));
+        Menu mn = new Menu();
+        mn.menu();
+        mn.toString();
         
-        //clear();
     }
 
-    public static void clear() {
-        try {
-            // Verifica se o sistema operacional é macOS ou outro
-            if (System.getProperty("os.name").contains("Mac")) {
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-            } else {
-                // Para outros sistemas, pode incluir mais condições, se necessário
-                System.out.println("Sistema operacional não suportado para limpar o terminal.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    
 }
--- antigos/Menu.java
+++ antigos/Menu.java
@@ -6,52 +6,44 @@ public class Menu {
         while (true) {
             
             System.out.print("----------\nOque quer fazer:\n1) Criar comanda \n2) Ver comandas\n3) Excluir item comanda\n4) Excluir comanda \n5) Modificar comanda\n0) encerrar\n_ _ _ _ _ _ _\nOque fazer: ");
-                String n=dd.next();
+            String n=dd.next();
             System.out.println("_ _ _ _ _ _ _");
             switch (n) {
                 case "1":
-                System.out.println("opção: Criar comanda!");
-
-
-                Thread.sleep(500);
-                break;
+                    System.out.println("opção: Criar comanda!");
+                    Thread.sleep(500);
+                    break;
                 case "2":
-                System.out.println("opção: Ver comanda");
-
-
-                Thread.sleep(500);
-                break;
+                    System.out.println("opção: Ver comanda");
+                    Thread.sleep(500);
+                    break;
                 case "3":
-                System.out.println("Opção: Excluir item de comanda");
-
-
-                Thread.sleep(500);
-                break;
+                    System.out.println("Opção: Excluir item de comanda");
+                    Thread.sleep(500);
+                    break;
                 case "4":
-                System.out.println("Opção: Excluir comanda");
-
-
-                Thread.sleep(500);
-                break;
+                    System.out.println("Opção: Excluir comanda");
+                    Thread.sleep(500);
+                    break;
                 case "5":
-                System.out.println("Modificar comanda");
-
-
-                Thread.sleep(500);
-                break;
+                    System.out.println("Modificar comanda");
+                    Thread.sleep(500);
+                    break;
                 case "0":
-                System.out.print("Encerando");
-                for(int i=0;i<3;i++){
-                    System.out.print(".");
-                    Thread.sleep(280);
-                }
-                System.out.println("");
-                System.exit(0);
+                    System.out.print("Encerando");
+                    for(int i=0;i<3;i++){
+                        System.out.print(".");
+                        Thread.sleep(280);
+                    }
+                    System.out.println("");
+                    System.exit(0);
                 default:
-                System.out.println("opção invalida!!");
-                
-                
+                    System.out.println("opção invalida!!");
             }
         }
+    }
+    @Override
+    public String toString(){
+            return "Entrou na class Menu";
     }
 }
